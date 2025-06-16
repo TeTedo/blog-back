625ba0ed610acab96202596aad0f4ab1d8f6180c
박태석 <qkrxotjr9508@gmail.com>
2025-06-16 21:19:55 +0900

    init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './module/post/post.module';
+import { CommentModule } from './module/comment/comment.module';
+import { AuthModule } from './module/auth/auth.module';
+import { AdminModule } from './module/admin/admin.module';
+import { FileModule } from './module/file/file.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule, CommentModule, AuthModule, AdminModule, FileModule],
   controllers: [AppController],
   providers: [AppService],
 })
